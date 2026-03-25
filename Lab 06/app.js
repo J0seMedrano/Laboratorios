0d9d43618cbcb4060b7845f4d576fdc295dca1a5
J0seMedrano <josecarlos.medrano@hotmail.com>
2026-03-25 17:53:51 -0600

    Lab07 agregando funciones

--- Lab 06/app.js
+++ Lab 06/app.js
@@ -1,12 +1,38 @@
-//importamos el cliente de supabase para interactuar con la base de datos
-// este cliente ya esta configurado con la url y la clave de acceso a nuestra instancia de supabase
+// importamos el cliente de Supabase para interactuar con la base de datos
+// este cliente ya está configurado con la URL y la clave de acceso a nuestra instancia de Supabase
 import { supabase } from "./supabase.js";
 
+// obtenemos referencias a los elementos del DOM que vamos a usar
+const txtSearch = document.getElementById("txtSearch");
 const btnLoad = document.getElementById("btnLoad");
 const tbody = document.getElementById("tbodyStudents");
 
-btnLoad.addEventListener("click", async () => {
-  const { data, error } = await supabase.from("estudiantes").select("id,nombre,apellido,correo,carrera");
+btnLoad.addEventListener("click", async () => consultarEstudiantes());
+
+// funcion de flecha
+// const consultarEstudiantes = async () => {};
+// funcion tradicional
+// function consultarEstudiantes() {}
+
+// let y const
+// let x = 10;
+// x = 20;
+// const y = 30;
+// y = 40; // error, no se puede reasignar una constante
+// var z = 50;
+// var z = 60; // no error, var permite redeclarar la misma variable
+
+const consultarEstudiantes = async () => {
+  // usamos el cliente de Supabase para hacer una consulta a la tabla "estudiantes"
+  // json: { "data": [], "error": null }
+  const search = txtSearch.value.trim() || ""; // si el campo de búsqueda está vacío, usamos una cadena vacía
+  const query = await supabase.from("estudiantes").select("id,nombre,apellido,correo,carrera");
+  if(search.length > 0) {
+        query.or(`nombre.ilike.%${search}%,apellido.ilike.%${search}%`);
+ 
+  }
+
+  const { data, error } = await query; // desestructuramos el resultado de la consulta para obtener los datos y el error
 
   if (error) {
     console.error(error);
@@ -16,6 +42,7 @@ btnLoad.addEventListener("click", async () => {
 
   tbody.innerHTML = "";
 
+  // data es un arreglo de objetos, cada objeto representa un estudiante
   data.forEach((r) => {
     const tr = document.createElement("tr");
     tr.innerHTML = `
@@ -25,8 +52,8 @@ btnLoad.addEventListener("click", async () => {
         <td>${r.correo ?? ""}</td>
         <td>${r.carrera ?? ""}</td>
       `;
+
     tbody.appendChild(tr);
   });
-});
-
+}; 
  
